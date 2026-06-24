7b62d9278cc72fcd389d5b92de6d7539ec26f181
Panagiota Gr <panagiotagrosdouli@gmail.com>
2026-06-24 21:18:55 +0300

    Make initial AegisVIO pipeline runnable

--- tests/test_uncertainty.py
+++ tests/test_uncertainty.py
@@ -7,9 +7,10 @@ def test_covariance_trace():
     assert covariance_trace(np.eye(3)) == 3.0
 
 
-def test_mahalanobis_distance_squared():
-    assert mahalanobis_distance_squared(np.array([1.0, 2.0]), np.eye(2)) == 5.0
-
+assert np.isclose(
+    mahalanobis_distance_squared(np.array([1.0, 2.0]), np.eye(2)),
+    5.0,
+)
 
 def test_uncertainty_score_decreases_with_quality():
     cov = np.eye(2) * 0.1
